16086ff2082881aed069ee8b2331823168b50252
BPCoder <mingyangcoder@163.com>
2024-12-11 10:19:29 +0800

    修改锁管理器

---
+++ lock/lock.go
@@ -0,0 +1,24 @@
+package lock
+
+import (
+	"context"
+	"github.com/bpcoder16/Chestnut/logit"
+	"github.com/bpcoder16/Chestnut/modules/lock/local"
+)
+
+var LocalManager *local.LockManager
+
+func init() {
+	LocalManager = local.NewLocalLockManager()
+}
+
+func CleanupLockManager(ctx context.Context) {
+	beforeCnt := LocalManager.Len()
+	if beforeCnt > 0 {
+		LocalManager.Cleanup()
+		afterCnt := LocalManager.Len()
+		logit.Context(ctx).InfoW("CleanupLocalLockManager", "success", "beforeCnt", beforeCnt, "afterCnt", afterCnt)
+	} else {
+		logit.Context(ctx).InfoW("CleanupLocalLockManager", "NoNeedToClean", "beforeCnt", beforeCnt)
+	}
+}
--- modules/lock/local/local.go
+++ modules/lock/local/local.go
@@ -7,89 +7,89 @@ import (
 
 const DelayExpireDuration = time.Minute * 15
 
-type localLock struct {
+type lock struct {
 	m           *sync.Mutex
 	expiredTime time.Time
 }
 
-func (l *localLock) lock() {
+func (l *lock) lock() {
 	l.m.Lock()
 }
 
-func (l *localLock) unlock() {
+func (l *lock) unlock() {
 	l.m.Unlock()
 }
 
-func (l *localLock) TryLock() bool {
+func (l *lock) TryLock() bool {
 	return l.m.TryLock()
 }
 
-func (l *localLock) IsExpired() bool {
+func (l *lock) IsExpired() bool {
 	return l.expiredTime.Add(DelayExpireDuration).Before(time.Now())
 }
 
-type LocalLockManager struct {
+type LockManager struct {
 	m  *sync.RWMutex
-	lm map[string]*localLock
+	lm map[string]*lock
 }
 
-func NewLocalLockManager() *LocalLockManager {
-	return &LocalLockManager{
-		lm: make(map[string]*localLock, 10000),
+func NewLocalLockManager() *LockManager {
+	return &LockManager{
+		lm: make(map[string]*lock, 10000),
 		m:  new(sync.RWMutex),
 	}
 }
 
-func (llm *LocalLockManager) getLocalLock(key string) (lock *localLock, isOK bool) {
+func (llm *LockManager) getLocalLock(key string) (lock *lock, isOK bool) {
 	llm.m.RLock()
 	defer llm.m.RUnlock()
 	lock, isOK = llm.lm[key]
 	return
 }
 
-func (llm *LocalLockManager) setLocalLock(key string, lock *localLock) {
+func (llm *LockManager) setLocalLock(key string, lock *lock) {
 	llm.m.Lock()
 	defer llm.m.Unlock()
 	llm.lm[key] = lock
 }
 
-func (llm *LocalLockManager) Lock(key string, expiredTime time.Time) {
-	lock, isOK := llm.getLocalLock(key)
+func (llm *LockManager) Lock(key string, expiredTime time.Time) {
+	localLock, isOK := llm.getLocalLock(key)
 	if !isOK {
-		lock = &localLock{
+		localLock = &lock{
 			m:           new(sync.Mutex),
 			expiredTime: expiredTime,
 		}
-		llm.setLocalLock(key, lock)
+		llm.setLocalLock(key, localLock)
 	}
-	lock.lock()
+	localLock.lock()
 }
 
-func (llm *LocalLockManager) TryLock(key string, expiredTime time.Time) {
-	lock, isOK := llm.getLocalLock(key)
+func (llm *LockManager) TryLock(key string, expiredTime time.Time) {
+	localLock, isOK := llm.getLocalLock(key)
 	if !isOK {
-		lock = &localLock{
+		localLock = &lock{
 			m:           new(sync.Mutex),
 			expiredTime: expiredTime,
 		}
-		llm.setLocalLock(key, lock)
+		llm.setLocalLock(key, localLock)
 	}
-	lock.TryLock()
+	localLock.TryLock()
 }
 
-func (llm *LocalLockManager) UnLock(key string) {
-	if lock, isOK := llm.getLocalLock(key); isOK {
-		lock.unlock()
+func (llm *LockManager) UnLock(key string) {
+	if localLock, isOK := llm.getLocalLock(key); isOK {
+		localLock.unlock()
 	}
 }
 
-func (llm *LocalLockManager) Len() int {
+func (llm *LockManager) Len() int {
 	llm.m.RLock()
 	defer llm.m.RUnlock()
 	return len(llm.lm)
 }
 
-func (llm *LocalLockManager) Cleanup() {
+func (llm *LockManager) Cleanup() {
 	llm.m.Lock()
 	defer llm.m.Unlock()
 	for key, lm := range llm.lm {
