82c591ec80964b842dbb1ccff4c2918fb19f8deb
BPCoder <mingyangcoder@163.com>
2024-12-11 09:49:36 +0800

    调整本地锁管理器

--- modules/lock/local/local.go
+++ modules/lock/local/local.go
@@ -1,4 +1,4 @@
-package block
+package local
 
 import (
 	"sync"
@@ -7,47 +7,47 @@ import (
 
 const DelayExpireDuration = time.Minute * 15
 
-type LocalLock struct {
+type localLock struct {
 	m           *sync.Mutex
 	expiredTime time.Time
 }
 
-func (l *LocalLock) Lock() {
+func (l *localLock) lock() {
 	l.m.Lock()
 }
 
-func (l *LocalLock) Unlock() {
+func (l *localLock) unlock() {
 	l.m.Unlock()
 }
 
-func (l *LocalLock) TryLock() bool {
+func (l *localLock) TryLock() bool {
 	return l.m.TryLock()
 }
 
-func (l *LocalLock) IsExpired() bool {
+func (l *localLock) IsExpired() bool {
 	return l.expiredTime.Add(DelayExpireDuration).Before(time.Now())
 }
 
 type LocalLockManager struct {
 	m  *sync.RWMutex
-	lm map[string]*LocalLock
+	lm map[string]*localLock
 }
 
 func NewLocalLockManager() *LocalLockManager {
 	return &LocalLockManager{
-		lm: make(map[string]*LocalLock),
+		lm: make(map[string]*localLock, 10000),
 		m:  new(sync.RWMutex),
 	}
 }
 
-func (llm *LocalLockManager) getLocalLock(key string) (lock *LocalLock, isOK bool) {
+func (llm *LocalLockManager) getLocalLock(key string) (lock *localLock, isOK bool) {
 	llm.m.RLock()
 	defer llm.m.RUnlock()
 	lock, isOK = llm.lm[key]
 	return
 }
 
-func (llm *LocalLockManager) setLocalLock(key string, lock *LocalLock) {
+func (llm *LocalLockManager) setLocalLock(key string, lock *localLock) {
 	llm.m.Lock()
 	defer llm.m.Unlock()
 	llm.lm[key] = lock
@@ -56,18 +56,30 @@ func (llm *LocalLockManager) setLocalLock(key string, lock *LocalLock) {
 func (llm *LocalLockManager) Lock(key string, expiredTime time.Time) {
 	lock, isOK := llm.getLocalLock(key)
 	if !isOK {
-		lock = &LocalLock{
+		lock = &localLock{
 			m:           new(sync.Mutex),
 			expiredTime: expiredTime,
 		}
 		llm.setLocalLock(key, lock)
 	}
-	lock.Lock()
+	lock.lock()
+}
+
+func (llm *LocalLockManager) TryLock(key string, expiredTime time.Time) {
+	lock, isOK := llm.getLocalLock(key)
+	if !isOK {
+		lock = &localLock{
+			m:           new(sync.Mutex),
+			expiredTime: expiredTime,
+		}
+		llm.setLocalLock(key, lock)
+	}
+	lock.TryLock()
 }
 
 func (llm *LocalLockManager) UnLock(key string) {
 	if lock, isOK := llm.getLocalLock(key); isOK {
-		lock.Unlock()
+		lock.unlock()
 	}
 }
 
