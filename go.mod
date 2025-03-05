ccdbac17e5c9c70b2acf45c57212aa77bff7bd2f
BPCoder <mingyangcoder@163.com>
2025-03-05 12:13:08 +0800

    feat: 替换方法支持 arm 环境

--- go.mod
+++ go.mod
@@ -20,6 +20,7 @@ require (
 	go.mongodb.org/mongo-driver v1.17.1
 	go.uber.org/zap v1.27.0
 	golang.org/x/sync v0.9.0
+	golang.org/x/sys v0.23.0
 	gorm.io/driver/clickhouse v0.6.1
 	gorm.io/driver/mysql v1.5.7
 	gorm.io/gorm v1.25.12
@@ -87,7 +88,6 @@ require (
 	golang.org/x/crypto v0.26.0 // indirect
 	golang.org/x/exp v0.0.0-20240613232115-7f521ea00fb8 // indirect
 	golang.org/x/net v0.27.0 // indirect
-	golang.org/x/sys v0.23.0 // indirect
 	golang.org/x/text v0.17.0 // indirect
 	golang.org/x/time v0.6.0 // indirect
 	google.golang.org/protobuf v1.34.1 // indirect
--- modules/zaplogger/hood_std.go
+++ modules/zaplogger/hood_std.go
@@ -2,14 +2,14 @@ package zaplogger
 
 import (
 	"github.com/bpcoder16/Chestnut/core/file/standard"
+	"golang.org/x/sys/unix"
 	"path"
-	"syscall"
 )
 
 // hookStderr 劫持 Stderr
 func hookStderr(logDir string) {
 	fh := standard.NewWriter(path.Join(logDir, "std", "stderr.log"))
-	err := syscall.Dup2(int(fh.Fd()), 2)
+	err := unix.Dup2(int(fh.Fd()), 2)
 	if err != nil {
 		panic("stderr.log syscall.Dup2 failed: " + err.Error())
 	}
@@ -18,7 +18,7 @@ func hookStderr(logDir string) {
 // hookStdout 劫持 Stdout
 func hookStdout(logDir string) {
 	fh := standard.NewWriter(path.Join(logDir, "std", "stdout.log"))
-	err := syscall.Dup2(int(fh.Fd()), 1)
+	err := unix.Dup2(int(fh.Fd()), 2)
 	if err != nil {
 		panic("stdout.log syscall.Dup2 failed: " + err.Error())
 	}
