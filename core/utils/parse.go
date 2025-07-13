e67196ce4533e158c5b193f6d4b505de84c1560e
BPCoder <mingyangcoder@163.com>
2025-07-13 20:44:34 +0800

    删除无用功能

--- core/utils/parse.go
+++ core/utils/parse.go
@@ -7,19 +7,6 @@ import (
 	"strings"
 )
 
-// ParseJSONFile
-// DEPRECATED: 请使用 ParseFile 替代
-func ParseJSONFile(path string, resPtr interface{}) (err error) {
-	v := viper.New()
-	v.SetConfigFile(path)
-	v.SetConfigType("json")
-	err = v.ReadInConfig()
-	if err == nil {
-		err = v.Unmarshal(resPtr)
-	}
-	return
-}
-
 func ParseFile(filePath string, resPtr interface{}) (err error) {
 	ext := filepath.Ext(filePath)
 	ext = strings.ToLower(ext)
