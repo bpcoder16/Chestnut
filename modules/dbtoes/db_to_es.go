3c7ff946883d83de234bdb42fec8e484e3351eb8
BPCoder <mingyangcoder@163.com>
2025-07-21 13:47:11 +0800

    添加默认灌入方式

--- modules/dbtoes/db_to_es.go
+++ modules/dbtoes/db_to_es.go
@@ -44,3 +44,35 @@ func TimePeriod(ctx context.Context, esManager *esclientv7.Manager, index string
 
 	return nil
 }
+
+func Default(ctx context.Context, esManager *esclientv7.Manager, index string, lastId uint64, esDataListFunc func(lastId uint64, limit int) (uint64, []any, error)) error {
+	var dataList []any
+	var err error
+	for {
+		lastId, dataList, err = esDataListFunc(lastId, limitNum)
+		if err != nil {
+			return err
+		}
+		if len(dataList) == 0 {
+			break
+		}
+		documentList := make([]esclientv7.Document, 0, len(dataList))
+		for _, v := range dataList {
+			esData := v
+			if esCommonItem, ok := esData.(ESCommonItem); ok {
+				documentList = append(documentList, esclientv7.Document{
+					ID:      esCommonItem.GetDocId(),
+					Content: esData,
+				})
+			}
+		}
+		if len(documentList) == 0 {
+			return errors.New("documentList.Empty")
+		}
+		if errES := esManager.BulkUpsert(ctx, index, documentList); errES != nil {
+			return errES
+		}
+	}
+
+	return nil
+}
