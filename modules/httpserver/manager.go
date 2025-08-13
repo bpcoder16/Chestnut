87851eb34e7d1372525927929662b6fa670b8615
BPCoder <mingyangcoder@163.com>
2025-08-13 13:40:27 +0800

    hotfix

--- modules/httpserver/manager.go
+++ modules/httpserver/manager.go
@@ -67,8 +67,10 @@ func (m *Manager) Run(ctx context.Context) error {
 		}
 
 		logit.Context(ctx).InfoW("httpServer.Manager.Run", "HttpServer shutdown...")
-		if err := m.server.Shutdown(ctx); err != nil {
-			logit.Context(ctx).ErrorW("httpServer.Manager.Run", "HttpServer shutdown failed:"+err.Error())
+		shutdownCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+		if err := m.server.Shutdown(shutdownCtx); err != nil {
+			logit.Context(ctx).ErrorW("httpServer.Manager.Run", "HttpServer shutdown failed: "+err.Error())
 		}
 		logit.Context(ctx).InfoW("httpServer.Manager.Run", "HttpServer shutdown completed, exited")
 	}()
